4c1788d2786ea695f964c922506802c6ba504a61
SAHA-YJB <pkomom123@naver.com>
2023-10-28 21:55:34 +0900

    features: common card component edit

--- src/components/features/ExpenseItem/ExpenseItem.jsx
+++ src/components/features/ExpenseItem/ExpenseItem.jsx
@@ -1,22 +1,23 @@
 import React from "react";
 import "../ExpenseItem/ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+import Card from "../../common/Card";
 
 const ExpenseItem = ({ expenses }) => {
   return (
-    <>
+    <Card className="expenses">
       {expenses.map((expense) => {
         return (
-          <div className="expense-item" key={expense.id}>
+          <Card className="expense-item" key={expense.id}>
             <ExpenseDate expenses={expense} />
             <div className="expense-item__description">
               <h2>{expense.title}</h2>
               <div className="expense-item__price">₩ {expense.amount}</div>
             </div>
-          </div>
+          </Card>
         );
       })}
-    </>
+    </Card>
   );
 };
 
