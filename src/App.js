fb6d314dc44bf9416849aa2cde6e9b6febc88289
SAHA-YJB <pkomom123@naver.com>
2023-10-28 15:14:11 +0900

    features: list render

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import ExpenseItem from "./components/features/ExpenseItem/ExpenseItem.jsx";
 
-function App() {
+const formatNumber = (num) => {
+  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+};
+const expenses = [
+  {
+    id: "e1",
+    title: "휴지",
+    amount: formatNumber(9000),
+    date: new Date().toLocaleDateString("ko-KR"),
+  },
+  {
+    id: "e2",
+    title: "TV",
+    amount: formatNumber(80000),
+    date: new Date().toLocaleDateString("ko-KR"),
+  },
+  {
+    id: "e3",
+    title: "닭 가슴살",
+    amount: formatNumber(10000),
+    date: new Date().toLocaleDateString("ko-KR"),
+  },
+  {
+    id: "e4",
+    title: "책상",
+    amount: formatNumber(450000),
+    date: new Date().toLocaleDateString("ko-KR"),
+  },
+];
+const App = () => {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem />
+      <ExpenseItem expenses={expenses} />
     </div>
   );
-}
+};
 
 export default App;
