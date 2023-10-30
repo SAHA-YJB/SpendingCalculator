2915747d5ad35c394dd0e7d60b788954c2d36616
SAHA-YJB <pkomom123@naver.com>
2023-10-30 16:31:47 +0900

    features: add expense

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import Expense from "./components/ExpenseItem/Expense";
 import NewExpense from "./components/NewExpense/NewExpense";
 
 const formatNumber = (num) => {
   return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 };
-const expenses = [
-  {
-    id: "e1",
-    title: "휴지",
-    amount: formatNumber(9000),
-    date: new Date(),
-  },
-  {
-    id: "e2",
-    title: "TV",
-    amount: formatNumber(80000),
-    date: new Date(),
-  },
-  {
-    id: "e3",
-    title: "닭 가슴살",
-    amount: formatNumber(10000),
-    date: new Date(),
-  },
-  {
-    id: "e4",
-    title: "책상",
-    amount: formatNumber(450000),
-    date: new Date(),
-  },
-];
+
 const App = () => {
+  const expenses = [
+    {
+      id: "e1",
+      title: "휴지",
+      amount: formatNumber(9000),
+      date: new Date(),
+    },
+    {
+      id: "e2",
+      title: "TV",
+      amount: formatNumber(80000),
+      date: new Date(),
+    },
+    {
+      id: "e3",
+      title: "닭 가슴살",
+      amount: formatNumber(10000),
+      date: new Date(),
+    },
+    {
+      id: "e4",
+      title: "책상",
+      amount: formatNumber(450000),
+      date: new Date(),
+    },
+  ];
+  const [expensesState, setExpensesState] = useState(expenses);
+
+  const addExpenseHandler = (expense) => {
+    console.log("In App.js");
+    console.log(expense);
+    return setExpensesState([expense, ...expensesState]);
+  };
   return (
     <>
-      <NewExpense />
-      <Expense expenses={expenses} />
+      <NewExpense addExpenseHandler={addExpenseHandler} />
+      <Expense expenses={expensesState} />
     </>
   );
 };
--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -1,21 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
+const ExpenseForm = ({ onSaveExpenseData }) => {
+  const [input, setInput] = useState({
+    title: "",
+    amount: "",
+    date: "",
+  });
+  const inputChangeHandler = (e) => {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const expenseData = {
+      ...input,
+      date: new Date(input.date),
+    };
+    onSaveExpenseData(expenseData);
+    setInput({
+      title: "",
+      amount: "",
+      date: "",
+    });
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
-          <label>Title</label>
-          <input type="text" />
+          <label>사용처</label>
+          <input
+            type="text"
+            name="title"
+            value={input.title}
+            onChange={inputChangeHandler}
+            placeholder="물건을 입력해주세요."
+          />
         </div>
         <div className="new-expense__control">
-          <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <label>금액</label>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            name="amount"
+            value={input.amount}
+            onChange={inputChangeHandler}
+            placeholder="금액을 입력해주세요."
+          />
         </div>
         <div className="new-expense__control">
-          <label>Date</label>
-          <input type="date" min="2019-01-01" max="2023-12-31" />
+          <label>날짜</label>
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2023-12-31"
+            name="date"
+            value={input.date}
+            onChange={inputChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
